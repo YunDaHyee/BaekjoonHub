0ae367e9a2128549d858bc1de6a9da05c7f56975
DDDDAD <7.yundaye.7@gmail.com>
2024-03-18 18:10:47 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.18 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,22 +1,30 @@
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
         // 분자 numer, 분모 denom
-        int[] answer = new int[2];
         int newNumer1,newNumer2,common,sumNumer;
 
-        newNumer1 = denom1==denom2?numer1:numer1*denom2;
-        newNumer2 = denom1==denom2?numer2:numer2*denom1;
-        common = denom1==denom2?denom1:denom1*denom2;
+        newNumer1 = numer1*denom2;
+        newNumer2 = numer2*denom1;
+        common = denom1*denom2;
 
         sumNumer = newNumer1 + newNumer2;
 
-        while( sumNumer%2==0&&common%2==0 ){
-            sumNumer/=2;
-            common/=2;
+         /*
+            while( sumNumer%2==0&&common%2==0 ){ // 2가 아니라 동시에 떨어질 숫자를 찾아야함..
+                sumNumer/=2;
+                common/=2;
+            }
+        */
+        
+        int max = 1; // 1은 모든 자연수의 공약수
+        
+        for( int i=1;i<=sumNumer&&i<=common;i++ ){
+            if( sumNumer%i==0&&common%i==0 ) {
+                max = i;
+            }
         }
 
-        answer[0] = sumNumer;
-        answer[1] = common;
+        int[] answer = { sumNumer/max, common/max }; // 최대공약수로 나눈 값
             
         return answer;
     }
