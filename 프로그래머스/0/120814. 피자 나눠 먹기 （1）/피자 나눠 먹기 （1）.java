f2c728dcab51432475963667e8e0953f1b3f06cf
DDDDAD <7.yundaye.7@gmail.com>
2024-03-20 22:09:20 +0900

    Update 피자 나눠 먹기 （1）.java

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,5 +1,23 @@
 class Solution {
     public int solution(int n) {
-        return n%7==0 ? n/7 : (n/7)+1;
+        // 1.
+        // int pizza = n/7;
+        // int spare = n%7;
+
+        // if( spare!=0 ){
+        //     pizza++;
+        //     for( ;spare>7;spare%=7 ){
+        //         pizza++;
+        //     }
+        // }
+
+        //return pizza;
+        
+        // 2.
+        // return (n + 6) / 7;
+        // 6 == 어떤 수를 7로 나눴을 때 0부터 나올 수 있는 나머지의 한계
+        
+        // 3.
+        // return n%7==0 ? n/7 : (n/7)+1;
     }
 }
