c5a10f078045182a847ab59c20a437c3f1c6673f
DDDDAD <7.yundaye.7@gmail.com>
2024-03-23 21:17:43 +0900

    Update 피자 나눠 먹기 （3）.java

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -1,5 +1,6 @@
 class Solution {
     public int solution(int slice, int n) {
+        // 1.
         int answer = n/slice;
         int nmg = n%slice;
         while( nmg!=0 ){
@@ -10,5 +11,12 @@ class Solution {
             nmg%=slice;
         }
         return answer;
+
+        // 2.
+        /*
+            사람수 (n) % 피자조각 (slice)의 나머지 > 0 => 피자를 한 판 더 시켜야 모두 한 조각 이상 먹을 수 있습니다.
+            => answer = 사람수 (n) / 피자조각 (slice) +1(피자 한 판)
+            return n % slice > 0 ? n/slice+1 : n/slice;
+        */
     }
 }
