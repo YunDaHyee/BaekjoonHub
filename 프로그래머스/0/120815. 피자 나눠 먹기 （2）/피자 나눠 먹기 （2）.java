fe0362ec32df44238ad018124ecd6c70343c9cd8
DDDDAD <7.yundaye.7@gmail.com>
2024-03-23 19:29:50 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,13 +1,13 @@
 class Solution {
     public int solution(int n) {
         // 6조각 잘라줌. 모두 같은 수의 피자 조각을 먹어야 한다면 최소 몇 판을 시켜야 하는지.
-        int answer = 1;
-        int remaining = 6;
-        while (remaining % n != 0) {
-            answer +=1;
-            remaining += 6;
-            
+        int a = n;
+        int b = 6;
+        while( b!=0 ){
+            int r = a%b;
+            a = b;
+            b = r;
         }
-        return answer;
+        return ((n*6)/a)/6;
     }
-}       
+}
