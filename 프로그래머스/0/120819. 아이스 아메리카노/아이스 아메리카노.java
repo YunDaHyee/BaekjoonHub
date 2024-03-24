a2e8145730a0b46ab20999019e7f7beac3d8654d
DDDDAD <7.yundaye.7@gmail.com>
2024-03-24 18:54:55 +0900

    Update 아이스 아메리카노.java

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,7 +1,11 @@
 class Solution {
     public int[] solution(int money) {
+        // 1.
         int count = money/5500;
         int remain = 5500*count;
         return new int[]{count, money-remain};
+
+        // 2.
+        // return new int[] { money / 5500, money % 5500 };
     }
 }
