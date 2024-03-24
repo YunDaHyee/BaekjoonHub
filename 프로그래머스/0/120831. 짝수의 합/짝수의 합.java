5cdd3e70ca32e7692eabd9e04daf0c4f6c820647
DDDDAD <7.yundaye.7@gmail.com>
2024-03-24 21:40:52 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -1,6 +1,14 @@
 import java.util.stream.IntStream;
 class Solution {
     public int solution(int n) {
-        return IntStream.rangeClosed(1,n).filter(i->i%2==0).sum();
+        // 1.
+        //return IntStream.rangeClosed(1,n).filter(i->i%2==0).sum();
+        
+        // 2.
+        int answer = 0;
+        for( int i=2;i<=n;i+=2 ){
+            answer += i;
+        }
+        return answer;
     }
 }
