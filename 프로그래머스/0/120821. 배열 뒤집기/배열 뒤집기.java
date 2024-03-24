f39749c2fea6877446e76bee443fe53ee7cbcb20
DDDDAD <7.yundaye.7@gmail.com>
2024-03-24 20:34:03 +0900

    [level 0] Title: 배열 뒤집기, Time: 2.15 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,5 +1,9 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int[] solution(int[] num_list) {
+        // 1.
+        /*
         int length = num_list.length;
         for( int i=0;i<num_list.length/2;i++ ){
             int temp = num_list[i];
@@ -7,5 +11,13 @@ class Solution {
             num_list[length-i-1] = temp;
         }
         return num_list;
+        */
+        
+        // 2.
+        // num_list랑 똑같은 길이를 가지는int 스트림 만들고 num_list의 뒤에 값부터 출력해서 Array로 리턴
+        return IntStream
+                    .range(1, num_list.length+1)
+                    .map(i -> num_list[num_list.length-i])
+                    .toArray();
     }
 }
