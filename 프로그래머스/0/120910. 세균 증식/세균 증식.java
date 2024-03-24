4f80f1d221965707ca627690d7eb14c4301d9632
DDDDAD <7.yundaye.7@gmail.com>
2024-03-24 22:13:01 +0900

    Update 세균 증식.java

--- 프로그래머스/0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -3,7 +3,7 @@ class Solution {
         // 1.
         // return n * (int)Math.pow(2,t);
         
-        // 2.
+        // 2. 비트연산으로 한칸씩 밀면 2를 곱한거랑 같은거니까 t만큼 밀어주는 것
         return n << t;
     }
 }
