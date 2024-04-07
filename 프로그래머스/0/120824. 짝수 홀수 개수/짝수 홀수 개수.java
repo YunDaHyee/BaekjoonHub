0c3cf9aac1b4a9641b9dc680307d225bbafc26a9
DDDDAD <7.yundaye.7@gmail.com>
2024-04-07 21:11:33 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.03 ms, Memory: 89.9 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -2,9 +2,20 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        return new int[]{
-                (int) Arrays.stream(num_list).filter(i->i%2==0).count()
-            , (int) Arrays.stream(num_list).filter(i->i%2!=0).count()
-            };
+        // 1.
+        // return new int[]{
+        //         (int) Arrays.stream(num_list).filter(i->i%2==0).count()
+        //     , (int) Arrays.stream(num_list).filter(i->i%2!=0).count()
+        // };
+        
+        int evenCount = 0;
+        int oddCount = 0;
+        
+        for( int i=0;i<num_list.length;i++ ){
+            if( num_list[i]%2==0 ) evenCount++;
+            else oddCount++;
+            
+        }
+        return new int[]{evenCount, oddCount};
     }
 }
