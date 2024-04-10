f7d5943632033aa76befe70cac22a85a7bfcd254
DDDDAD <7.yundaye.7@gmail.com>
2024-04-10 20:05:59 +0900

    Update 특정 문자 제거하기.java

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -3,6 +3,10 @@ import java.util.stream.Collectors;
     
 class Solution {
     public String solution(String my_string, String letter) {
-        return Arrays.stream(my_string.split("")).filter(i->!i.equals(letter)).collect(Collectors.joining());
+        // 1.
+        // return Arrays.stream(my_string.split("")).filter(i->!i.equals(letter)).collect(Collectors.joining());
+        
+        // 2.
+        return my_string.replace(letter, "");
     }
 }
