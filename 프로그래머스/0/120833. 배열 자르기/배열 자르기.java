9ff8477071316fc99d2c835dda220b4f62456f93
DDDDAD <7.yundaye.7@gmail.com>
2024-04-13 17:43:34 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = new int[num2-num1+1];
+        for( int i=num1,j=0;i<=num2;i++,j++ ){
+           answer[j]=numbers[i];
+        }
+        return answer;
+    }
+}
