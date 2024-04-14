077186019c3bc8b250728929a65c7e4a1b615f96
DDDDAD <7.yundaye.7@gmail.com>
2024-04-14 14:26:44 +0900

    Update 외계행성의 나이.java

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,15 +1,26 @@
 class Solution {
     public String solution(int age) {
         StringBuilder answer = new StringBuilder();
-        char[] cs = new char[10];
-        char c='a';
-        for( int i=0;i<10;i++ ){
-            cs[i]=c++;
+        
+       // 1.
+       /*
+       char[] cs = new char[10];
+       char c='a';
+       for( int i=0;i<10;i++ ){
+           cs[i]=c++;
+       }
+       String[] parsedAges = String.valueOf(age).split("");
+       for( String parsedAge : parsedAges ){
+           answer.append( cs[Integer.parseInt(parsedAge)] );
+       }
+       */
+
+        // 2.
+        while(age>0){
+            answer.insert( 0, (char)(age%10+(int)'a') );// int형으로 아스키값에 대한 10진수를 구함
+            age/=10;
         }
-        String[] parsedAges = String.valueOf(age).split("");
-        for( String parsedAge : parsedAges ){
-            answer.append( cs[Integer.parseInt(parsedAge)] );
-        }
+        
         return answer.toString();
     }
 }
