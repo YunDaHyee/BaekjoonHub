2fad39d762740d9b315b9bcd1612329e3d2b1504
DDDDAD <7.yundaye.7@gmail.com>
2024-04-27 19:03:47 +0900

    Update 순서쌍의 개수.java

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -24,5 +24,22 @@ class Solution {
         }
         
         return orderedPair.size()*2-duplicationNumberCount;
+
+        // 2.
+        /*
+            if(n < 2)
+                return n;
+            int answer = 2; // 1과 자기자신
+            for(int i=2; i<Math.sqrt(n+1); i++){
+                if(n % i == 0){
+                    answer += i == n / i ? 1 : 2;
+                }
+            }
+            return answer;
+        */
+
+        // 3. O(n)이지만 스트림 이용
+        // return (int) IntStream.rangeClosed(1, n).filter(i -> n % i == 0).count();
+        
     }
 }
